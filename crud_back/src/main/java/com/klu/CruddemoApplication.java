654ebe63b6833f3a2fb659c3b6f999dde4254b33
KOLLA SAI SIVA RAMA KRISHNA <2300031410@kluniversity.in>
2025-08-31 13:11:19 +0530

    Full Stack

--- crud_back/src/main/java/com/klu/CruddemoApplication.java
+++ crud_back/src/main/java/com/klu/CruddemoApplication.java
@@ -2,6 +2,7 @@ package com.klu;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 
 @SpringBootApplication
 public class CruddemoApplication {
@@ -9,5 +10,9 @@ public class CruddemoApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(CruddemoApplication.class, args);
 	}
+
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(CruddemoApplication.class);
+    }
 
 }
